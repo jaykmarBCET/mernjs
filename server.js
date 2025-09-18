63d9f65aa815436b83f009b885f12fabb9354e74
jaykmarBCET <jaykumar3dear@gmail.com>
2025-09-18 21:25:08 +0530

    Fix bugs

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 import Root from "./roots/roots.config.js";
+import fs from 'node:fs'
+import chalk from "chalk";
 
 dotenv.config();
 
@@ -16,14 +18,21 @@ async function startServer() {
   app.use("/api", Root);
 
   if (process.env.MODE === "production") {
-    
+
     const distPath = path.join(__dirname, "dist");
 
-    
+
     app.use(express.static(distPath));
 
-    
-    app.get("/", (req, res) => {
+
+    app.get("/", async (req, res) => {
+
+      try {
+         fs.access(distPath); // checks if folder exists & is accessible
+      } catch (err) {
+        console.log("Run `npm run build` first!");
+        throw new Error("Please build first");
+      }
       res.sendFile(path.join(distPath, "index.html"));
     });
   } else {
